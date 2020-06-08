4cc1d1bab0e9f22be7f1c8ba63b36f3052b8679e
judyact <judylee.act@gmail.com>
2020-06-08 18:45:44 -0500

    Add hamburger button to header; adjust index.html, style.css.

---
+++ hamburger_button.js
@@ -0,0 +1,269 @@
+/* App.jsx */
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      menuOpen:false,
+    }
+  }
+  
+  handleMenuClick() {
+    this.setState({menuOpen:!this.state.menuOpen});
+  }
+  
+  handleLinkClick(val) {
+    this.setState({menuOpen: false});
+    console.log(val);
+    
+    if (val.localeCompare('Resume') == 0) {
+      window.location.href="https://drive.google.com/open?id=16-aZPhOv5ggCu-FTMC1cBfrJRGwt8D7pOwJuTZY7i7E";
+    } else if (val.localeCompare('Voice Over') == 0) {
+      window.location.href="https://www.backstage.com/u/judy-lee/media/audio/";
+    } else if (val.localeCompare('Writing') == 0) {
+      window.location.href="https://medium.com/@judylee.act";
+    };
+    // window.location.href="http://google.com";
+  }
+  
+  render(){
+    const styles= 
+      {
+        container:{
+          position: 'absolute',
+          top: 2,
+          left: 20,
+          zIndex: '99',
+          opacity: 1,
+          display:'flex',
+          alignItems:'center',
+          background: 'transparent',
+          width: '50%',           // JL: Adjust this when re-arranging header links
+          color: 'pink',
+          fontFamily:'Lobster',
+        },
+        body: {
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          width: '100vw',
+          height: '100vh',
+          filter: this.state.menuOpen ? 'blur(2px)':null,
+          transition: 'filter 0.5s ease',
+        },        
+      }
+    const menu = ['Resume', 'Voice Over', 'Writing']
+    const menuItems = menu.map((val,index)=>{
+      return (
+        <MenuItem 
+          key={index} 
+          delay={`${index * 0.1}s`}
+          onClick={()=>{this.handleLinkClick(val);}}>{val}</MenuItem>)
+    });
+    
+    return(
+      <div>
+        <div style={styles.container}>
+          <MenuButton open={this.state.menuOpen} onClick={()=>this.handleMenuClick()} color='black'/>
+        </div>
+        <Menu open={this.state.menuOpen}>
+          {menuItems}
+        </Menu>
+      </div>
+    )
+  }
+}
+
+/* MenuItem.jsx*/
+class MenuItem extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      hover:false,
+    }
+  }
+  
+  handleHover(){
+    this.setState({hover:!this.state.hover});
+  }
+  
+  render(){
+    const styles={
+      container: {
+        opacity: 0,
+        animation: '1s appear forwards',
+        animationDelay:this.props.delay,
+      },
+      menuItem:{
+        fontFamily:`'Open Sans', sans-serif`,
+        fontSize: '1.2rem',
+        padding: '1rem 0',
+        margin: '0 5%',
+        cursor: 'pointer',
+        color: this.state.hover? 'blue':'black',
+        transition: 'color 0.2s ease-in-out',
+        animation: '0.5s slideIn forwards',
+        animationDelay:this.props.delay,
+
+      },
+      line: {
+        width: '90%',
+        height: '1px',
+        background: 'pink',
+        margin: '0 auto',
+        animation: '0.5s shrink forwards',
+        animationDelay:this.props.delay,
+        
+      }
+    }
+    return(
+      <div style={styles.container}>
+        <div 
+          style={styles.menuItem} 
+          onMouseEnter={()=>{this.handleHover();}} 
+          onMouseLeave={()=>{this.handleHover();}}
+          onClick={this.props.onClick}
+        >
+          {this.props.children}  
+        </div>
+      <div style={styles.line}/>
+    </div>  
+    )
+  }
+}
+
+/* Menu.jsx */
+class Menu extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      open: this.props.open? this.props.open:false,
+    }
+  }
+    
+  componentWillReceiveProps(nextProps){
+    if(nextProps.open !== this.state.open){
+      this.setState({open:nextProps.open});
+    }
+  }
+  
+  render(){
+    const styles={
+      container: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        height: this.state.open? '100%': 0,
+        width: '100vw',
+        display: 'flex',
+        flexDirection: 'column',
+        background: 'transparent',
+        opacity: 1,
+        color: 'black',
+        transition: 'height 0.3s ease',
+        zIndex: 2,
+      },
+      menuList: {
+        paddingTop: '3rem',
+        backgroundColor: '#f542b0',
+      }
+    }
+    return(
+      <div style={styles.container}>
+        {
+          this.state.open?
+            <div style={styles.menuList}>
+              {this.props.children}
+            </div>:null
+        }
+      </div>
+    )
+  }
+}
+
+/* MenuButton.jsx */
+class MenuButton extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      open: this.props.open? this.props.open:false,
+      color: this.props.color? this.props.color:'black',
+    }
+  }
+
+  componentWillReceiveProps(nextProps){
+    if(nextProps.open !== this.state.open){
+      this.setState({open:nextProps.open});
+    }
+  }
+  
+  handleClick(){
+  this.setState({open:!this.state.open});
+  }
+  
+  render(){
+    const styles = {
+      container: {
+        height: '32px',
+        width: '32px',
+        display:'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        cursor: 'pointer',
+        padding: '4px',
+      },
+      line: {
+        height: '2px',
+        width: '20px',
+        background: this.state.color,
+        transition: 'all 0.2s ease',
+      },
+      lineTop: {
+        transform: this.state.open ? 'rotate(45deg)':'none',
+        transformOrigin: 'top left',
+        marginBottom: '5px',
+      },
+      lineMiddle: {
+        opacity: this.state.open ? 0: 1,
+        transform: this.state.open ? 'translateX(-16px)':'none',
+      },
+      lineBottom: {
+        transform: this.state.open ? 'translateX(-1px) rotate(-45deg)':'none',
+        transformOrigin: 'top left',
+        marginTop: '5px',
+      },       
+    }
+    return(
+      <div style={styles.container} 
+        onClick={this.props.onClick ? this.props.onClick: 
+          ()=> {this.handleClick();}}>
+        <div style={{...styles.line,...styles.lineTop}}/>
+        <div style={{...styles.line,...styles.lineMiddle}}/>
+        <div style={{...styles.line,...styles.lineBottom}}/>
+      </div>
+    )
+  }
+}
+
+/* Main.jsx */
+class Main extends React.Component {
+  render(){
+    const styles = {
+      main: {
+        display:'flex',
+        flexDirection:'column',
+        alignItems: 'center',
+        height: '100vh',
+      }
+    }
+    
+    return (
+      <div style={styles.main}>
+        <App />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+     <Main/>,document.querySelector('#hamburger'));
