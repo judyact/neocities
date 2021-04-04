7eb807d683ffd3c5fc09ed768b429f6efef364e6
judyact <judylee.act@gmail.com>
2021-04-04 17:28:10 -0500

    Update height on the resume.

--- hamburger_button.js
+++ hamburger_button.js
@@ -16,7 +16,7 @@ class App extends React.Component {
     console.log(val);
     
     if (val.localeCompare('Resume') == 0) {
-      window.location.href="https://drive.google.com/file/d/1JO_yzbxhvVVqwJQ514AhSd1uvVPqlhDD/view?usp=sharing";
+      window.location.href="https://drive.google.com/file/d/1RKBYbGrk_UatJnvVpd-W1dMw24eiP-A7/view?usp=sharing";
     } else if (val.localeCompare('Voice Over') == 0) {
       window.location.href="https://www.backstage.com/u/judy-lee/media/audio/";
     } else if (val.localeCompare('Writing') == 0) {
