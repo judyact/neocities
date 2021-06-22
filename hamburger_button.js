f0b339eba1e46864331315f90cf246091aa8973d
judyact <judylee.act@gmail.com>
2021-06-22 13:15:04 -0500

    Update resume on the hamburger button and on the show case.

--- hamburger_button.js
+++ hamburger_button.js
@@ -16,7 +16,7 @@ class App extends React.Component {
     console.log(val);
     
     if (val.localeCompare('Resume') == 0) {
-      window.location.href="https://drive.google.com/file/d/1RKBYbGrk_UatJnvVpd-W1dMw24eiP-A7/view?usp=sharing";
+      window.location.href="https://drive.google.com/file/d/1ihIfrBX18ibvXbxUkZsoHzHVhBR9TQ-N/view?usp=sharing";
     } else if (val.localeCompare('Voice Over') == 0) {
       window.location.href="https://www.backstage.com/u/judy-lee/media/audio/";
     } else if (val.localeCompare('Writing') == 0) {
